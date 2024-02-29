194346e385b890fe311a5c2b032f8999acaa42ca
jurayevcoder <jurayevcoder@gmail.com>
2024-02-29 10:39:22 +0500

    section 4 and edit section

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -2,12 +2,12 @@
     <section>
         <div class="container mx-auto 2xl:px-0 px-5">
             <div class="2xl:pt-40 pt-40 relative z-10">
-                <div class="box px-5 py-20 rounded-3xl flex items-center justify-between">
-                    <div class="flex flex-col items-start sm:w-[50%] gap-10"
+                <div class="box px-5 py-20 rounded-3xl flex items-center justify-center">
+                    <div class="flex flex-col items-center  gap-10"
                         :class="{ ' text-white': navbar.userNav, ' text-[#12111F]': !navbar.userNav }">
-                        <h2 class="uppercase sm:text-2xl text-lg font-bold text-[#4ec3e0]">virtual ta'lim</h2>
-                        <div class="flex flex-col items-start gap-5">
-                            <h1 class="hero-title lg:text-7xl sm:text-6xl text-3xl font-bold ">Biz bilim
+                        <h2 class="uppercase sm:text-2xl text-md font-bold text-[#4ec3e0]">virtual ta'lim</h2>
+                        <div class="flex flex-col items-center gap-5">
+                            <h1 class="hero-title 2xl:text-8xl lg:text-7xl sm:text-6xl text-2xl font-bold text-center">Biz bilim
                                 olishingizga ko'maklashamiz!</h1>
                             <p class=" sm:text-base text-xs">
                                 Istalgan joyda, o'zingizga qulay vaqtda o'qish imkoniyati..
@@ -56,9 +56,10 @@
         </div>
     </section>
 
-    <section class="relative z-10 sm:pt-20 pt-10" :class="{ ' text-white': navbar.userNav, ' text-[#12111F]': !navbar.userNav }">
+    <section class="relative z-10 sm:pt-20 pt-10"
+        :class="{ ' text-white': navbar.userNav, ' text-[#12111F]': !navbar.userNav }">
         <div class="container mx-auto 2xl:px-0 px-5">
-            <div class="box rounded-3xl p-5">
+            <div class="box rounded-3xl px-5 py-10">
                 <h2 class="sm:text-3xl text-2xl font-bold mb-10">Nima uchun "Virtual Ta'lim" da o’qish kerak ?</h2>
                 <div class="grid lg:grid-cols-2 gap-5">
                     <div class="box rounded-3xl p-10 flex sm:flex-row flex-col sm:items-center sm:gap-5 gap-3">
@@ -108,6 +109,34 @@
             </div>
         </div>
     </section>
+
+    <section class="bg-[#4ec3e0] relative sm:mt-20 mt-10 z-10 overflow-x-hidden">
+        <div class="container mx-auto px-5">
+            <div class="items flex items-center justify-between gap-3 py-5" data-v-e33b1926="">
+                <p class="entry text-white sm:text-base text-sm">Birinchi qadam sizdan</p>
+                <p class="entry text-white sm:text-base text-sm">•</p>
+                <p class="entry text-white sm:text-base text-sm">Birinchi qadam sizdan</p>
+                <p class="entry text-white sm:text-base text-sm">•</p>
+                <p class="entry text-white sm:text-base text-sm">Birinchi qadam sizdan</p>
+                <p class="entry text-white sm:text-base text-sm">•</p>
+                <p class="entry text-white sm:text-base text-sm">Birinchi qadam sizdan</p>
+                <p class="entry text-white sm:text-base text-sm">•</p>
+                <p class="entry text-white sm:text-base text-sm">Birinchi qadam sizdan</p>
+                <p class="entry text-white sm:text-base text-sm">•</p>
+                <p class="entry text-white sm:text-base text-sm">Birinchi qadam sizdan</p>
+                <p class="entry text-white sm:text-base text-sm">•</p>
+                <p class="entry text-white sm:text-base text-sm">Birinchi qadam sizdan</p>
+                <p class="entry text-white sm:text-base text-sm">•</p>
+                <p class="entry text-white sm:text-base text-sm">Birinchi qadam sizdan</p>
+                <p class="entry text-white sm:text-base text-sm">•</p>
+                <p class="entry text-white sm:text-base text-sm">Birinchi qadam sizdan</p>
+                <p class="entry text-white sm:text-base text-sm">•</p>
+                <p class="entry text-white sm:text-base text-sm">Birinchi qadam sizdan</p>
+                <p class="entry text-white sm:text-base text-sm">•</p>
+                <p class="entry text-white sm:text-base text-sm">Birinchi qadam sizdan</p>
+            </div>
+        </div>
+    </section>
 </template>
 
 <script setup>
@@ -117,23 +146,21 @@ const navbar = useNavStore();
 
 <style lang="css" scoped>
 .items[data-v-e33b1926] {
-    width: -moz-fit-content;
-    width: fit-content;
-    width: -moz-max-content;
-    width: max-content;
-    animation: 20s linear 0s infinite normal none running animation-1l6vhht;
-    overflow-x: auto;
-    transition: animation-play-state 0.3s ease 0s
+  width: -moz-fit-content;
+  width: fit-content;
+  width: -moz-max-content;
+  width: max-content;
+  animation: carouselAnim-e33b1926 10s infinite alternate linear;
+  overflow-x: auto;
 }
 
-@keyframes animation-1l6vhht {
-    0% {
-        transform: translateX(0px);
-    }
-
-    100% {
-        transform: translateX(-100%);
-    }
+@keyframes carouselAnim-e33b1926 {
+  0% {
+    transform: translate(0);
+  }
+  100% {
+    transform: translate(calc(-100% + 1500px));
+  }
 }
 
 .hero-title {
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -46,18 +46,16 @@ const navbar = useNavStore();
 <style>
 
 .area {
-    background: -webkit-linear-gradient(to left, #8f94fb, #4e54c8);
     width: 100%;
-
-
 }
 
 .circles {
     position: absolute;
     top: 0;
     left: 0;
+    bottom: 0;
     width: 100%;
-    height: 100%;
+    height: 100vh;
     /* overflow: hidden; */
 }
 
