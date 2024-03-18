b5bc81713e7bb61dac114ea325ea429fa40236b5
jurayevcoder <jurayevcoder@gmail.com>
2024-03-18 10:06:55 +0500

    profil edit

--- src/views/Profil/Profil.vue
+++ src/views/Profil/Profil.vue
@@ -2,23 +2,22 @@
     <section class="2xl:pt-40 pt-40 relative z-10"
         :class="{ ' text-white': navbar.userNav, ' text-[#12111F]': !navbar.userNav }">
         <div class="container mx-auto px-5">
-            <div class="box rounded-3xl flex flex-col items-center p-5">
-                <div class="flex items-center gap-10 border rounded-3xl border-[#4ec3e0] w-[800px] p-5">
-                    <img class="sm:w-80 sm:h-80 w-40 h-40" src="/user.png" alt="">
-                    <div class="flex flex-col gap-1 w-full">
-                        <h2 class="sm:text-5xl font-bold">John Doe</h2>
-                        <span class="sm:text-base text-[#4ec3e0] font-medium">O'quvchi</span>
-                        <div class="flex items-center justify-between mt-10">
-                            <span class="flex items-center gap-1 sm:text-base font-medium">
-                                <i class='bx bx-envelope text-2xl text-red-500'></i>
+            <div class=" rounded-3xl flex flex-col items-center">
+                <div class="box flex items-center justify-between rounded-3xl w-full p-5">
+                    <div class="flex items-center gap-5">
+                        <img class="sm:w-20 sm:h-20 w-40 h-40" src="/user.png" alt="">
+                        <div class="flex flex-col gap-1">
+                            <h2 class="sm:text-3xl font-bold">John Doe</h2>
+                            <a
+                                class="flex items-center gap-1 sm:text-sm font-medium hover:underline cursor-pointer hover:text-[#4ec3e0]">
                                 johndoe@gmail.com
-                            </span>
-                            <span class="flex items-center gap-1 sm:text-base font-medium">
-                                <i class='bx bx-phone text-2xl text-green-500'></i>
-                                +998901234567
-                            </span>
+                            </a>
                         </div>
                     </div>
+                    <button class="flex items-center gap-1 border px-7 py-3 text-base rounded-full border-[#4ec3e0] text-[#4ec3e0] hover:bg-[#4ec3e0] hover:text-white">
+                        <i class='bx bxs-edit-alt text-xl'></i>
+                        O'zgartirish
+                    </button>
                 </div>
 
                 <div class="border-b border-[#4ec3e0] w-full mt-5 flex items-center justify-center gap-20 pb-3">
